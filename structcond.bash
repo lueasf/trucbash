1d2b7621b1488fd487860d8ba361c9f3a7b23fff
CORREIA Lucie <lucie.correia@telecomnancy.eu>
2023-12-22 22:42:22 +0100

    boucle

--- structcond.bash
+++ structcond.bash
@@ -15,3 +15,19 @@ else
     echo "le nombre est inferieur a 5";
 fi
 
+# rappel AND && et OR ||
+test -x /usr/bin/firefox && echo "firefox est executable"
+test -x /usr/bin/firefox || echo "firefox n'est pas executable"
+# si le test 1 echoue , le test 2 est execute
+
+#comment executer un bloc de code en fonction d'une valeur de var:
+case $VAR in
+    pattern1) ...;;
+    pattern2) ...;;
+esac
+#bien mettre ;;
+prenom="code"
+case $prenom in
+    *co*) true;;
+    *) false;;
+esac
